fde1bc05bbcef4ea3194b58d89abd1c893894d38
Jealous Matsikachando <pasej5@gmail.com>
2025-09-19 01:30:09 +0200

    portf

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('insights/', views.blog, name='insights'),
     path('contact/', views.contact, name='contact'),
-    path('portfolio/', views.portfolio, name='portfolio'),
+    path('portfolio_home/', views.portfolio, name='portfolio_home'),
     path('careers/', views.careers, name='careers'),
     path('services/', views.services, name='services'),
 ]
