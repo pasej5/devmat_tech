2844930570978d1ffca9fecd7d1c64f105e0093d
Jealous Matsikachando <pasej5@gmail.com>
2025-09-19 01:44:33 +0200

    port views

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'portfolio'
+
 urlpatterns = [
-    path('', views.portfolio_home, name='portfolio_home'),
+    path('', views.index, name='index'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def portfolio_home(request):
+def index(request):
     return render(request, 'portfolio/index.html')
