88e45a675c823d6b8ab69a338cb5a7f485d973a1
Jealous Matsikachando <pasej5@gmail.com>
2025-09-19 01:38:00 +0200

    portf

--- jmatsika_website/settings.py
+++ jmatsika_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4_elssie20mn)o=amfa57h@0ukzh1qdlo9n^fy45-+7)c)#z4i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['64.23.140.67', 'localhost', '127.0.0.1']
 
 
 # Application definition
