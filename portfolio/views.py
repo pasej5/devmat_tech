12dd35768f50aed6baffa5596472d91a443900ae
Jealous Matsikachando <pasej5@gmail.com>
2025-11-19 08:59:33 +0200

    toast notification added

--- portfolio/views.py
+++ portfolio/views.py
@@ -26,20 +26,8 @@ def index(request):
         except Exception as e:
             logger.error(f"send_mail raised an exception: {e}")
 
-        return redirect("/portfolio")   # Prevent form resubmission
+           # Prevent form resubmission
+        return render(request, "portfolio/index.html", {"success": True})
+    
     return render(request, "portfolio/index.html")
 
-def contact_view(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        email = request.POST.get("email")
-        message = request.POST.get("message")
-
-        subject = f"New message from {name}"
-        body = f"From: {name}\nEmail: {email}\n\n{message}"
-
-        send_mail(subject, body, None, ["your_personal_email@gmail.com"])
-
-        return render(request, "index.html", {"success": True})
-
-    return render(request, "index.html")
